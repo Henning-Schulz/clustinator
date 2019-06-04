4fbf6d5cc2791825ce1446089d158c54baed5c3d
An Dang <andang@hotmail.de>
2019-06-04 23:35:21 +0200

    CONT-190: Backpropagate the results to the session

--- src/clustering.py
+++ src/clustering.py
@@ -39,57 +39,60 @@ class Clustering:
 if __name__ == '__main__':
     # Input data
     input = Input(sessions_file)
-
-    start, stop = None, 1000
-    past_step_size = 1000
-    steps = 5
-    for _ in enumerate(range(steps)):
+    print('load data done', datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'))
+    start, stop = 0, 1000
+    step_size = 500
+    iteration = 5
+    diff_dict = {}
+    for _ in enumerate(range(iteration)):
+        print("Iteration:", _)
         if _ == (0, 0):
             pass
         else:
-            start += 1000
-            stop += 1000
+            start += step_size
+            stop += step_size
 
         # Slice sessions @sessions(None, None)
-        current_step = input.sessions(start, stop)
+        next_step = input.sessions(start, stop)
 
         if _ >= (1, 1):
-            past_start = (start-past_step_size)
-            past_stop = (stop-past_step_size)
+            past_start = (start-step_size)
+            past_stop = (stop-step_size)
             past_step = input.sessions(past_start, past_stop)
 
-            print('load data done', datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'))
+            # Compute transition matrix next
+            mc_next = MarkovChain(next_step, states)
+            mc_next = mc_next.transition_matrix()
 
-
-            """
-            To Do's:
-            Change the parameter names!!!
-            """
-            # Compute transition matrix
-            mc = MarkovChain(session, states)
-            mc = mc.transition_matrix()
+            # Compute transition matrix past
+            mc_past = MarkovChain(past_step, states)
+            mc_past = mc_past.transition_matrix()
 
             print('matrix done', datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'))
-            print('start clustering \n')
+            print('start clustering')
 
             # DBSCAN
-            dbscan = Clustering(mc)
+            dbscan_next = Clustering(mc_next)
+            dbscan_past = Clustering(mc_past)
 
-            print("\nEnd clustering", datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'))
+            print("End clustering", datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'), '\n')
 
-            """# Compute the clustering compromise
-            result1 = dbscan.compare_results()
-            result2 = dbscan.compare_results()
-            
-            diff_dict = {}
+            # Compute the clustering compromise
+            result1 = dbscan_next.compare_results()
+            result2 = dbscan_past.compare_results()
+            print('result_1:', result1)
+            print('result_2:', result2)
+
             tmp = []
-            
+
             for key in result1:
                 tmp.append(key)
                 if key in result2:
                     diff_dict[key] = (result1[key] - result2[key])
-        
+
             if len(tmp) != len(result2):
                 for key in result2:
                     if key not in result1.keys():
-                        diff_dict[key] = result2[key]"""
+                        diff_dict[key] = result2[key]
+            print(diff_dict)
+            break
