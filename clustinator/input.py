eed0838ea703260c9fb844ab31ef11c68fdfbf1a
Henning Schulz <henning.schulz@novatec-gmbh.de>
2020-01-15 18:23:15 +0100

    CONT-303: Enable triggering all clusterings from a certain starting point.

--- clustinator/input.py
+++ clustinator/input.py
@@ -19,11 +19,12 @@ class Input:
     def get_header(self):
         header_dict = {}
         header_list = ['app-id', 'version', 'tailoring', 'append-strategy',
-                       'start-micros', 'interval-start-micros', 'end-micros']
+                       'start-micros', 'interval-start-micros', 'end-micros', 'continuation']
         for header in header_list:
             for k, v in self.data.items():
                 if k == header:
                     header_dict[k] = v
+        
         return header_dict
 
     def get_prev_markov_chain(self):
@@ -42,8 +43,8 @@ class Input:
         return self.data['lookback']
     
     def get_append_strategy(self):
-        return self.data['append-strategy']
+        return self.data.get('append-strategy')
     
     def get_dimensions(self):
-        return self.data['dimensions']
+        return self.data.get('dimensions')
     
