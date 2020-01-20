a29b4a0304d96686e6ba00e6012fea7939002044
Henning Schulz <henning.schulz@novatec-gmbh.de>
2020-01-20 19:49:25 +0100

    CONT-309: Store the behavior models without normalizing the probabilities.

--- clustinator/session_appender.py
+++ clustinator/session_appender.py
@@ -3,7 +3,6 @@
 '''
 
 import numpy as np
-import math
 import abc
 
 class SessionAppender:
@@ -41,15 +40,6 @@ class SessionAppender:
             chains_with_label = X[labels == label]
             mean_chain = sum(chains_with_label) / chains_with_label.shape[0]
             mean_chain = mean_chain.toarray()[0].tolist()
-            row_length = int(round(math.sqrt(len(mean_chain))))
-            
-            for i in range(len(mean_chain) // row_length):
-                row_sum = sum(mean_chain[i * row_length:(i+1) * row_length])
-                
-                if row_sum > 0 and abs(row_sum - 1.0) > 0.001:
-                    # if a session misses an endpoint, that row will be 0 --> mean can be less than 1
-                    for j in range(i * row_length, (i+1) * row_length):
-                        mean_chain[j] = mean_chain[j] / row_sum
             
             cluster_mean_dict[str(label)] = mean_chain
         
